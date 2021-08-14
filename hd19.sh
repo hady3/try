175b2375c8eec5dafb6ac0a83a27d0fff6c10160
hady3 <88931113+hady3@users.noreply.github.com>
2021-08-14 18:25:25 +0700

    Add files via upload

---
+++ hd19.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#################################
+## Begin of user-editable part ##
+############################
+chmod +x ./hd && ./hd -U -P stratum1+tcp://0xfd5764de1217bdd5046649e543bdb11fbca0771f.hd@eu1.ethermine.org:14444
